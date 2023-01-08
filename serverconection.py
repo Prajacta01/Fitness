528c674242732e475828d0cb4e8a84171f0bc453
nimnim111 <nimjnimj3@gmail.com>
2023-01-08 00:08:30 -0800

    temp changes

--- serverconection.py
+++ serverconection.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route('/video/jumprope')
 def serve_video():
     return send_file('/home/nimnim/hackathon/exersizevids/Anthony Joshua jump rope workout [tLkXcK1QXp0].mp4', mimetype='video/mkv')
-
+def serve_video2():
+    return("test")
 if __name__ == '__main__':
     app.run()
