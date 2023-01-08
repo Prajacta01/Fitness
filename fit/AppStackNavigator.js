9c1117e814a74cc85dd52f7ada69df4586c0f7c9
Prajacta <prajacta@Prajactas-MacBook-Air.local>
2023-01-07 18:35:12 -0800

    object detection works, home screen

--- fit/AppStackNavigator.js
+++ fit/AppStackNavigator.js
@@ -1,12 +1,20 @@
+import React from 'react'
 import { createStackNavigator } from "@react-navigation/stack";
 
 import ImageDetection from "./pages/ImageDetection";
+import Home from "./pages/Home"
 
 const Stack = createStackNavigator();
 
 const AppStackNavigator = () => {
     return (
         <Stack.Navigator>
+            <Stack.Screen
+                initialRouteName="Home"
+                name="Home"
+                component={Home}
+                options={{ headerShown: false }}
+            />
             <Stack.Screen
                 name="ImageDetection"
                 component={ImageDetection}
--- fit/pages/ImageDetection.js
+++ fit/pages/ImageDetection.js
@@ -1,11 +1,70 @@
-import { View, Text } from 'react-native';
+import { StyleSheet, FlatList, Button, Platform, View, Text } from 'react-native';
+import { cameraWithTensors } from '@tensorflow/tfjs-react-native'
+import React, { useEffect, useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import * as mobilenet from '@tensorflow-models/mobilenet';
+// import { TFHUB_SEARCH_PARAM } from '@tensorflow/tfjs-converter/dist/executor/graph_model';
+import { Camera } from 'expo-camera'
+import * as tf from '@tensorflow/tfjs'
 
-const ImageDetection = () => {
-    return (
-        <View>
-            <Text>here</Text>
-        </View>
-    )
+const TensorCamera = cameraWithTensors(Camera);
+
+
+export default function ImageDetection() {
+  const [model, setModel] = useState(mobilenet.MobileNet)
+
+  let textureDims = Platform.OS == 'ios'
+    ? { height: 1920, width: 1080 }
+    : { height: 1200, width: 1600 };
+
+  function handleCameraStream(images) {
+    const loop = async () => {
+      const nextImageTensor = images.next().value;
+      if (!model || !nextImageTensor)
+        throw new Error('No model or image tensor');
+      model.detect(nextImageTensor).then((prediction) => {
+
+      })
+    };
+    loop();
+  }
+
+  const initialiseTensorflow = async () => {
+    await tf.ready();
+    tf.getBackend();
+  }
+
+  useEffect(() => {
+    console.log('starting')
+    const load = async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      console.log(status, 'status');
+    };
+    load();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <TensorCamera
+        style={styles.camera}
+        type={Camera.Constants.Type.back}
+        cameraTextureHeight={textureDims.height}
+        cameraTextureWidth={textureDims.width}
+        resizeHeight={200}
+        resizeWidth={152}
+        resizeDepth={3}
+        onReady={() => { }}
+        autorender={true}
+        useCustomShadersToResize={false}
+      />
+    </View>
+  );
 }
 
-export default ImageDetection
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backfround: '#fff',
+  },
+  camera: {},
+});
